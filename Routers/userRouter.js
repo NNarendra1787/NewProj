78968250bca5b22e7717419d593003bda3e86055
Narendra <narendranarwade2002@gmail.com>
2023-08-23 09:13:30 +0530

    lat

--- Routers/userRouter.js
+++ Routers/userRouter.js
@@ -1,14 +1,15 @@
-const { Accesseries, Total, Laptops, Mobiles } = require('../Controllers/Category');
+const { Accesseries, Total, Laptops, Mobiles, AllinOne } = require('../Controllers/Category');
 const { register, login } = require('../Controllers/userControler');
 const authorization = require('../midd');
 
 const userRouter = require('express').Router();
 
 userRouter.post('/register', register)
+userRouter.post('/login',login)
+userRouter.get('/All', AllinOne)
 userRouter.get('/Acc', Accesseries)
 userRouter.get('/mob', Mobiles)
 userRouter.get('/lap', Laptops)
 userRouter.get('/total', Total)
-userRouter.post('/login',login)
 
 module.exports = userRouter
